e0ee77631e2acb9412f10d8e923d39e339cdd623
nmgd-17 <nmg@nmgd-20.newmediaguruorg.com>
2017-09-15 18:50:53 +0530

    Routing setup

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
     { path: '**', component: HomeComponent }
 ];
 
